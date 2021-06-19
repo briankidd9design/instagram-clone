52ece4407aee3c7fc86296e9ba785bad8563b504
Brian Kidd <brian.kidd.one@gmail.com>
2021-06-19 09:24:16 -0700

    programmed UI forsignin and login pages

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # instagram-react12
+
+## Routes (pages)
+
+- / (feed page)
+
+Components:
+
+- FeedPost
+- FeedPostSkeleton
+- FeedSideSuggestions
+
+- /explore (explore page)
+
+Components:
+
+- ExploreSuggestions
+- ExploreGrid
+
+- /p/:postId (post page)
+
+Components:
+
+- Post
+- PostSkeleton
+- PostModal
+- MorePostsFromUser
+
+- notifications
+
+Components:
+
+- NotificationList
+- NotificationToolTip
+
+- /:username (profile page)
+
+Components:
+
+- ProfileTabs
+
+- /accounts/edit/ (edit profile page)
+
+- /accounts/login (login page)
+
+- /accounts/emailsignup/ (signup page)
+
+- - (not found page)
+
+## Share Components
+
+- Navbar
+- FollowSuggestions
+- UserCard
+- FollowButton
+- LoadingScreen
+- OptionsDialog
+- ProfilePicture
+- Layout
+- SEO
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,10 +1,98 @@
 import React from "react";
 import { useSignUpPageStyles } from "../styles";
+import SEO from "../components/shared/Seo";
+import { Card, TextField, Button, Typography } from "@material-ui/core";
+import { LoginWithFacebook } from "./login";
+import { Link } from "react-router-dom";
 
 function SignUpPage() {
-  useSignUpPageStyles();
+  const classes = useSignUpPageStyles();
 
-  return <div>SignUpPage</div>;
+  return (
+    <React.Fragment>
+      <SEO title="Sign up" />
+      <section className={classes.section}>
+        <article>
+          <Card className={classes.card}>
+            <div className={classes.cardHeader} />
+            <Typography className={classes.cardHeaderSubHeader}>
+              Sign up to see photos and videos from your friends.
+            </Typography>
+            {/* <LoginWithFacebook /> */}
+            <LoginWithFacebook
+              color="primary"
+              iconColor="white"
+              variant="contained"
+            />
+
+            <div className={classes.orContainer}>
+              <div className={classes.orLine} />
+              <div>
+                <Typography variant="body2" color="textSecondary">
+                  OR
+                </Typography>
+              </div>
+              <div className={classes.orLine} />
+            </div>
+
+            <form>
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Email"
+                type="email"
+                margin="dense"
+                className={classes.textField}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Full Name"
+                margin="dense"
+                className={classes.textField}
+                autocomplete="username"
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Username"
+                margin="dense"
+                className={classes.textField}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Password"
+                type="password"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="new-password"
+              />
+              <Button
+                variant="contained"
+                fullWidth
+                color="primary"
+                className={classes.button}
+                type="submit"
+              >
+                Sign Up
+              </Button>
+            </form>
+          </Card>
+          <Card className={classes.loginCard}>
+            <Typography align="right" variant="body2">
+              Have an account?
+            </Typography>
+            <Link to="/accounts/login">
+              <Button color="primary" className={classes.loginButton}>
+                Log in
+              </Button>
+            </Link>
+          </Card>
+        </article>
+      </section>
+    </React.Fragment>
+  );
 }
 
 export default SignUpPage;
