f43dbaac140d4f0e6214861fd2d274f7444b2ec3
Brian Kidd <brian.kidd.one@gmail.com>
2021-06-24 10:49:57 -0700

    created feed page

--- src/components/feed/FeedSideSuggestions.js
+++ src/components/feed/FeedSideSuggestions.js
@@ -1,10 +1,43 @@
 import React from "react";
 import { useFeedSideSuggestionsStyles } from "../../styles";
+import { getDefaultUser } from "../../data";
+import { Paper, Typography } from "@material-ui/core";
+import FollowButton from '../shared/FollowButton';
+import UserCard from '../shared/UserCard';
+import { LoadingIcon } from "../../icons";
 
-function FeedSideSuggestions() {
-  useFeedSideSuggestionsStyles();
 
-  return <div>FeedSideSuggestions</div>;
+function FeedSideSuggestions({ user }) {
+  const classes = useFeedSideSuggestionsStyles();
+
+  let loading = false;
+
+  return (
+    <article className={classes.article}>
+      <Paper className={classes.paper}>
+        <Typography
+          color="textSecondary"
+          variant="subtitle2"
+          component="h2"
+          align="left"
+          gutterBottom
+          className={classes.typography}
+        >
+          Suggestions for You
+        </Typography>
+        {loading ? (
+          <LoadingIcon />
+        ) : (
+          Array.from({ length: 5 }, () => getDefaultUser()).map(user => (
+            <div key={user.id} className={classes.card}>
+              <UserCard user={user} />
+              <FollowButton side />
+            </div>
+          ))
+        )}
+      </Paper>
+    </article>
+  );
 }
 
 export default FeedSideSuggestions;
